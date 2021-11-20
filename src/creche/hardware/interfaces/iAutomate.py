5e4f785557e30f14b31f548c54ea07686791ebcf
Whalend42 <whalend42@gmail.com>
2021-11-20 14:18:20 +0100

    remove incoherent method in interface

--- src/creche/hardware/interfaces/iAutomate.py
+++ src/creche/hardware/interfaces/iAutomate.py
@@ -2,14 +2,6 @@ from abc import ABCMeta, abstractmethod
 
 class IAutomate(metaclass=ABCMeta):
 
-    #@abstractmethod
-    #def start(self):
-    #    pass
-
-    @abstractmethod
-    def terminate(self):
-        pass
-
     @abstractmethod
     def play(self):
         pass
